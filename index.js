091940571b033101baf775272982c82b910df738
Raine Revere <raine@cybersemics.org>
2020-11-11 14:53:04 -0700

    comment

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /** Wraps a value in a function. If given a function, returns it as-is. */
 const wrap = x => typeof x === 'function' ? x : () => x
 
+/** Returns true if all arguments are true or evaluate to true. */
 const and = (...fs) => (...args) =>
   fs.length === 0 || (
     !!wrap(fs[0])(...args) &&
     and(...fs.slice(1))(...args)
   )
 
+/** Returns true if at least one argument is true or evaluates to true. */
 const or = (...fs) => (...args) =>
   fs.length > 0 && (
     !!wrap(fs[0])(...args) ||
