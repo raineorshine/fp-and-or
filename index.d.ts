d1b5f365cf122c197f6bcaff95f73eb7d2082872
Raine Revere <raine@cybersemics.org>
2023-09-27 16:00:03 +0000

    Fix type.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
-const Predicate = () => boolean
-export function and(...fs: (Predicate | boolean)[]): (...args: any[]) => boolean
-export function or(...fs: (Predicate | boolean)[]): (...args: any[]) => boolean
+type Predicate = () => boolean;
+
+export function and(
+  ...fs: (Predicate | boolean)[]
+): (...args: any[]) => boolean;
+
+export function or(...fs: (Predicate | boolean)[]): (...args: any[]) => boolean;
