aa983e8e0ee5efa076d3278e42df96602b26c045
Raine Revere <raine@cybersemics.org>
2023-11-05 03:22:03 +0000

    Add boolean to param type.

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
-export function and<T extends any[]>( ...fs: ((...args: T) => boolean)[] ): (...fs: T) => boolean;
-export function or<T extends any[]>( ...fs: ((...args: T) => boolean)[] ): (...fs: T) => boolean;
+type Predicate<T extends any[]> = (...args: T) => boolean;
+
+export function and<T extends any[]>( ...fs: (Predicate<T>|boolean)[] ): (...fs: T) => boolean;
+export function  or<T extends any[]>( ...fs: (Predicate<T>|boolean)[] ): (...fs: T) => boolean;
